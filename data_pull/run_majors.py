bfbea9033910986bca32344f83d6ac27267bfb24
voli <amcclernon@student.unimelb.edu.au>
2019-08-31 01:13:22 +1000

    add majors requirements finder

--- data_pull/run_majors.py
+++ data_pull/run_majors.py
@@ -1,8 +1,29 @@
 from scrapers.Majors import MajorsScraper
 from scrapers.MajorRequirements import MajorRequirementsScraper
+from collections import deque
+import time
+import json
 
 
-scraper = MajorRequirementsScraper()
-scraper.run("computing-and-software-systems")
-# s = MajorsScraper()
-# s.run("bachelor-of-agriculture", "undergraduate")
+ug = "undergraduate"
+
+def local_run():
+    queue = deque()
+    scraper = MajorRequirementsScraper()
+    course = ""
+
+    with open(r'majors/undergraduate/bachelor-of-biomedicine.json', 'r') as code_file:
+        f = json.load(code_file)
+        course = f['course']
+        for item in f['data']:
+            queue.append(item)
+
+    while queue:
+        if scraper.busy:
+            time.sleep(2)
+        else:
+            major = queue.popleft().replace(" ", "-").lower()
+            scraper.run("undergraduate", course, major)
+
+if __name__ == "__main__":
+    local_run()
